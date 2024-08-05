3018abfd4e59bf9d14f3be8686c2e5824da35d78
ysgiraldo <ygiraldoca@unal.edu.co>
2024-08-04 23:38:59 -0500

    Se modulariza mucho más el orquestador

--- src/prueba_sgc/steps.py
+++ src/prueba_sgc/steps.py
@@ -5,9 +5,18 @@ import numpy as np
 import os
 import json
 import psycopg2
+from funciones import Step
 
-
-class ExtractTransformLoad():
+"""
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+-- Fecha Creación: 2024-08-01
+-- Autores: Sofia Giraldo
+-- Descripción: Ejecución y calendarización de la rutina
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+"""
+class ExtractTransformLoad(Step):
     def ejecutar(self):
         # cfg = self.getStepConfigUtf8()
         cfg = self.getStepConfig()
@@ -39,55 +48,6 @@ class ExtractTransformLoad():
         df = pd.DataFrame(results, columns=column_names)
 
         print(df.head())
-
-
-
-    def getFolderPath(self):
-        return os.path.join(os.path.dirname(__file__), 'static', '')
-
-    def getStepConfig(self):
-
-        path = self.getFolderPath() + "config.json"
-
-        if os.path.exists(path):
-            with open(path, 'r', encoding='utf-8') as f_in:
-                json_str = f_in.read()
-            
-            # Se lee el archivo config.json
-            cfg = json.loads(json_str)
-
-            # Se define que llame lass variables del nombre de la clase
-            cfg = cfg.get(type(self).__name__,{})
-
-            return cfg
-        
-    def getSQLPath(self):
-
-        return os.path.join(os.path.dirname(__file__), 'static', 'sql', type(self).__name__, '')
-
-    def connectDataBase(self):
-        cfg = self.getStepConfig()
-
-        try:
-            connection = psycopg2.connect(
-                user= cfg["credenciales"]["user"],
-                password=cfg["credenciales"]["password"],
-                host=cfg["credenciales"]["host"],
-                port=cfg["credenciales"]["port"],
-                database=cfg["credenciales"]["database"]
-            )
-            cursor = connection.cursor()
-            cursor.execute("SET client_encoding TO 'UTF8';")
-            print("Codificación del cliente configurada a UTF8")
-
-            # Ejemplo de ejecución de una consulta
-            cursor.execute("SELECT version();")
-            record = cursor.fetchone()
-            print(f"Versión de PostgreSQL: {record}")
-            return connection, cursor
-        except (Exception, psycopg2.Error) as error:
-            print("Error al conectar a PostgreSQL", error)
-            return None, None
 
 class Prueba():
     def ejecutar(self):
